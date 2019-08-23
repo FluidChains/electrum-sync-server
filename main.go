ffd80ecbbabd0542ed1a9d212ccda61597451c05
Karim Boucher <boucherkarim@gmail.com>
2019-08-23 17:35:49 +0000

    Update postgres conn details

--- main.go
+++ main.go
@@ -13,16 +13,16 @@ const (
 	Version = "0.0.4"
 )
 
-var dbType = envflag.String("DBTYPE", "sqlite3", "Database to use, valid options sqlite3, mysql or postgres")
+var dbType = envflag.String("DBTYPE", "postgres", "Database to use, valid options sqlite3, mysql or postgres")
 var dbDir = envflag.String("DBDIR", defaultDbDir(), "Sqlite3 - Path where the application should look for the database file.")
 
-var user = envflag.String("DBUSER", "root", "postgres/mysql - user name")
+var user = envflag.String("DBUSER", "postgres", "postgres/mysql - user name")
 var password = envflag.String("DBPASSWORD", "''", "postgres/mysql - password")
-var host = envflag.String("DBHOST", "127.0.0.1", "postgres/mysql - hostname")
+var host = envflag.String("DBHOST", "db_labelsync", "postgres/mysql - hostname")
 var port = envflag.Int("DBPORT", 5432, "postgres/mysql - port")
-var db = envflag.String("DBDATABASE", "electrum-label-sync", "postgres/mysql - database name")
+var db = envflag.String("DBDATABASE", "postgres", "postgres/mysql - database name")
 
-var listenPort = envflag.String("LISTENPORT", "0.0.0.0:8080", "Port where the json api should listen at in host:port format.")
+var listenPort = envflag.String("LISTENPORT", "localhost:8080", "Port where the json api should listen at in host:port format.")
 
 var useTLS = envflag.Bool("useTls", false, "Serve json api conncetions over TLS.")
 var certPath = envflag.String("certPath", "cert.pem", "Path to TLS certificate")
